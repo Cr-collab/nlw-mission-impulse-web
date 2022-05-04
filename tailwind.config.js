e00f286a0bcbae20aa2dc07f20cc59532c801ff1
Cristiano Azevdo <cristiccorrea@gmail.com>
2022-05-04 11:04:04 -0300

    libs e imagens necessarias

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,14 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          300: '#8257e2',
           500: '#8257e6'
         }
+      },
+      borderRadius: {
+        md: '4px'
       }
     }
   },
-  plugins: []
+  plugins: [require('@tailwindcss/forms'), require('tailwind-scrollbar')]
 }
